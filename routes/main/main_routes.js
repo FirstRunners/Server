aad9143342bb414d5ba60d92ee525078b870ba54
minkyoe <ddal0924@naver.com>
2018-10-05 19:23:11 +0900

    DB Date 변수 String으로 변경 // FIN main/check // ADD main/graph

--- routes/main/main_routes.js
+++ routes/main/main_routes.js
@@ -5,5 +5,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/check', require('../main/check'));
+router.use('/graph', require('../main/graph'));
 
 module.exports = router;
