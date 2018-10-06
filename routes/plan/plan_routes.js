4825d094f5630e11389cdcfa6d6864d38abddb64
minkyoe <ddal0924@naver.com>
2018-10-07 08:15:36 +0900

    MOD plan/now result & FIN plan/past

--- routes/plan/plan_routes.js
+++ routes/plan/plan_routes.js
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.use('/new', require('../plan/new'));
 router.use('/now', require('../plan/now'));
+router.use('/past', require('../plan/past'));
 
 module.exports = router;
